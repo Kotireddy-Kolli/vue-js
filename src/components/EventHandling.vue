a300ae382c462a51c3cea5009e3858672a665bf9
Kotireddy-Kolli <kotireddykolli1998@gmail.com>
2022-12-03 20:22:15 +0530

    initial commit

---
+++ src/components/EventHandling.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+    <h3>{{name}}</h3>
+    <div>
+        <button v-on:click="name='Button name changed'">Change Name</button>
+    </div>
+    </div>
+</template>
+
+<script> 
+export default {
+    name: 'EventHandling',
+    data(){
+        return {
+            name:'event'
+        }
+    },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+</style>
---
+++ src/components/ListRender.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+    <!-- Array of strings-->
+    <h3 v-for="name in names" v-bind:key="name">{{name}}</h3>
+    <h2>Render using index</h2>
+    <h3 v-for="(name,index) in names" v-bind:key="index">{{index}}.{{name}}</h3>
+
+    <!-- Array of Obj-->
+    <h2>Render using Array of Obj</h2>
+    <h3 v-for="name in objnames" :key="name.name">{{name.name}}</h3>
+
+    <!-- Rendering Obj-->
+    <h2>Render using Array of Obj</h2>
+    <h3 v-for="(value,key,index) in greetings" :key="index">{{value}},{{key}},{{index}}</h3>
+
+    <!-- Methods -->
+    <h3>Multiple Method: {{multiply(2)}}</h3>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'ListRender',
+    data(){
+        return {
+            names: ['Hi','Hey','Hello'],
+            objnames:[
+                {name:'Hi'},
+                {name:'Hey'},
+                {name:'Hello'},
+            ],
+            greetings:{
+                greeting1:"Hi",
+                greeting2:"Hey",
+                greeting3:'Hello'
+            },
+            baseMultiplier:5,
+        }
+    },
+    methods:{
+        multiply(num){
+            return num*this.baseMultiplier
+        }
+    },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
