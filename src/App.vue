73bf816c7a405f1366e29a9e2da563afe59eea4f
Kotireddy-Kolli <kotireddykolli1998@gmail.com>
2022-12-03 20:21:17 +0530

    feat: Initial commit

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<template>
+
+<div>
+  <div>First Name:{{firstname}}</div>
+  <!-- <div v-text="lastname">Lastname:</div> //Throw an error-->
+  <div v-text="lastname"></div>
+  <div >Lastname:{{boldlastname}}</div>
+  <div v-html="boldlastname"></div>
+
+  <!-- Binding Attributes--->
+  <h3 v-bind:id="heading">Heading h3</h3>
+  <button v-bind:disabled="isDisabled">Bind</button>
+
+  <!-- Binding Static Classes-->
+  <div class="underlined">This Text Shoud be underlined</div>
+
+  <!-- Binding Dynamic Classes-->
+  <div v-bind:class="status">Color change Based on status </div>
+
+  <!-- Binding Styles-->
+
+  <h2 v-bind:style="{
+    color: highlightColor,
+    fontSize: headerSize+ 'px',
+    padding:'20px',
+  }">Inline Styles</h2>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      firstname: "Koti",
+      lastname: "Reddy",
+      boldlastname: "<b>Reddy</b>",
+      heading: "heading",
+      isDisabled: false,
+      status:"accepted",
+      highlightColor:"Blue",
+      headerSize:50,
+    };
+  },
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.underlined{
+  text-decoration: underline;
+}
+.accepted{
+  color: green;
+}
+.rejected{
+  color: red;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+//import App from './App.vue'
+//import ListRender from "./components/ListRender"
+import EventHandling from './components/EventHandling'
+
+//createApp(App).mount('#app')
+//createApp(ListRender).mount('#app')
+createApp(EventHandling).mount('#app')
